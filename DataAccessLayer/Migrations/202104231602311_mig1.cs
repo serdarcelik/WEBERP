3a8366f64d1630e3d52756003bf498c42c2c34a5
serdarcelik <serdarce@hotmail.com>
2021-04-24 15:45:25 +0300

    Initial commit

---
+++ DataAccessLayer/Migrations/202104231602311_mig1.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.contents", "WriterID", c => c.Int());
+            CreateIndex("dbo.contents", "WriterID");
+            AddForeignKey("dbo.contents", "WriterID", "dbo.Writers", "WriterID");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.contents", "WriterID", "dbo.Writers");
+            DropIndex("dbo.contents", new[] { "WriterID" });
+            DropColumn("dbo.contents", "WriterID");
+        }
+    }
+}
